7836ef70d4ce7ee728cb84b54ca43cdada1db7a0
dh711 <dhairya711@gmail.com>
2021-11-16 10:01:20 +0530

    added functions.

--- lr0-parser.py
+++ lr0-parser.py
@@ -23,25 +23,50 @@ def append_dot(productions):
 	for production in productions:
 		production.replace('->', '->.')
 
-# def closure()
+def find_closure(item):
+	c = [item]
+	for item in c:
+		dot_pos = item.index('.')
+		if dot_pos != len(item)-1:
+			target = dot_pos + 1
+			for production in productions:
+				if production[0] == item[target] and append_dot(production) not in c:
+					c.append(append_dot(production))
 
-# def goto()
+	return c
 
-# def generate_automaton(productions):
+def goto(item):
+	temp = []
+	dot_pos = item.index('.')
+	if dot_pos < len(item)-1:
+		new_item = item[0:dot_pos] + item[dot_pos+1] + '.' + item[dot_pos+2:]
+		if new_item.index('.') == len(new_item) - 1:
+			temp.append(new_item)
+			return temp
+		else:
+			c = find_closure(new_item)
+			return c
+	else:
+		return item
+
+def find_next_states(states, symbols):
+	pass
 
 # def generate_table(states)
 
 productions = ['S->AA', 'A->aA', 'A->b']
 
-print('Grammer:')
+print('Grammar:')
 print(productions)
 
 terminals = get_terminals(productions)
 non_terminals = get_non_terminals(productions)
+symbols = terminals + non_terminals
 
 print('\nTerminals & Non-Terminals:')
 print(terminals)
 print(non_terminals)
+print(symbols)
 
 augment(productions)
 print('\nAugmented Grammar:')
@@ -49,7 +74,12 @@ print(productions)
 
 append_dot(productions)
 
-# states = generate_automaton(productions)
+# states = [[1, productions]]
+# find_next_states(states)
+# print('\nStates:')
+# print(states)
+
+
 # table = generate_table(states)
 
 # Print table.
