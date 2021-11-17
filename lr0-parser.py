bc33ab5f1d83afdb7773f5108a98937f0317341b
dh711 <dhairya711@gmail.com>
2021-11-17 21:14:02 +0530

    added functions, cleaned code.

--- lr0-parser.py
+++ lr0-parser.py
@@ -1,20 +1,25 @@
-def get_terminals(productions):
-	terminals = []
-	for production in productions:
-		if production[0] not in terminals and production[0].isupper():
-			terminals.append(production[0]) 
-
-	return terminals
+import termtables as tt
 
 def get_non_terminals(productions):
 	non_terminals = []
 	for production in productions:
-		temp = production.split('->')
-		for char in temp[1]:
-			if char not in non_terminals and char.islower():
-				non_terminals.append(char) 
+		lhs = production.split('->')[0]
+		rhs = production.split('->')[1]
+		for char in lhs+rhs:
+			if char not in non_terminals and char.isupper():
+				non_terminals.append(char)
 
 	return non_terminals
+
+def get_terminals(productions, non_terminals):
+	terminals = []
+	for production in productions:
+		temp = production.split('->')[1]
+		for char in temp:
+			if char not in non_terminals and char not in non_terminals and char != '|':
+				terminals.append(char) 
+
+	return terminals
 
 def find_closure(items, G):
 	closure = items
@@ -33,10 +38,10 @@ def find_closure(items, G):
 	
 	return closure
 
-def find_canonical_items(G, symbols):
+def find_canonical_items(G, symbols, start_symbol):
 	canonical_items = []
 	state_transitions = []
-	canonical_items.append(find_closure([['X', '.S$']], G))
+	canonical_items.append(find_closure([['X', '.' + str(start_symbol) + '$']], G))
 
 	for itemset in canonical_items:
 		for symbol in symbols:
@@ -67,44 +72,121 @@ def find_canonical_items(G, symbols):
 			
 			if (new_state not in canonical_items):
 				if (goto):
-					state_transitions.append(['g',canonical_items.index(itemset)+1,len(canonical_items)+1,symbol])
+					state_transitions.append(['GOTO',canonical_items.index(itemset)+1,len(canonical_items)+1,symbol])
 				
 				elif (shift):
-					state_transitions.append(['s',canonical_items.index(itemset)+1,len(canonical_items)+1,symbol])
+					state_transitions.append(['SHIFT',canonical_items.index(itemset)+1,len(canonical_items)+1,symbol])
 				
 				canonical_items.append(new_state)
 
 			else:
 				if (goto):
-					state_transitions.append(['g', canonical_items.index(itemset)+1, canonical_items.index(new_state)+1, symbol])
+					state_transitions.append(['GOTO', canonical_items.index(itemset)+1, canonical_items.index(new_state)+1, symbol])
 				elif (shift):
-					state_transitions.append(['s', canonical_items.index(itemset)+1, canonical_items.index(new_state)+1, symbol])
+					state_transitions.append(['SHIFT', canonical_items.index(itemset)+1, canonical_items.index(new_state)+1, symbol])
 
 	return canonical_items, state_transitions
 
-productions = ['S->AA', 'A->aA|b']
+def make_reductions(canonical_items, G, start_symbol):
+	accept_state = -1
+	reductions = [ [] for i in range(len(canonical_items)) ]
+	final_item = ['X', str(start_symbol)+'.$']
+	final_rules = []
+
+	# Create rules with dot on the right end for checking.
+	for lhs in G.keys():
+		for rhs in G[lhs]:
+			final_rules.append([lhs, rhs + str('.')])
+
+	for itemset in canonical_items:
+		if (final_item in itemset):
+			accept_state = canonical_items.index(itemset)+1
+
+		for item in itemset:
+			if (item in final_rules):
+				reductions[canonical_items.index(itemset)].append(final_rules.index(item))
+
+	return accept_state, reductions
+
+def createParseTable(state_transitions, reductions, terminals, non_terminals):
+	i = 0
+	symbols = terminals + non_terminals
+	symbolMap = dict()
+	parseTable = [ ['-' for i in range(len(symbols))] for j in range(len(canonical_items)) ]
+
+	for symbol in symbols:
+		symbolMap[symbol] = i
+		i = i+1
+
+	for s in state_transitions:
+		if (s[0][0] == 'S'):
+			parseTable[s[1]-1][symbolMap[s[3]]] = s[0][0]+str(s[2]-1)
+		else:
+			parseTable[s[1]-1][symbolMap[s[3]]] = str(s[2]-1)
+
+	parseTable[accept_state-1][symbolMap['$']] = 'Accepted'
+
+	for i in reductions:
+		if (len(i) > 0):
+			for j in terminals:
+				parseTable[reductions.index(i)][symbolMap[j]] = 'R' + str(i[0])
+
+	return parseTable
+
+def printTable():
+	parseTable = createParseTable(state_transitions, reductions, terminals, non_terminals)
+
+	table = []
+
+	header = [''] * (len(terminals) + 1)
+	header[(len(terminals) + 1) // 2] = 'Action'
+
+	header2 = [''] * len(non_terminals)
+	header2[(len(non_terminals)) // 2] = 'Goto'
+
+	table.append([''] + terminals + non_terminals)
+	for line_no, line in enumerate(parseTable):
+		table.append([line_no+1] + line)
+
+	data = [''] * (len(terminals) + len(non_terminals))
+
+	final_table = tt.to_string(data=table, header=header + header2, style=tt.styles.ascii_thin_double, padding=(0, 1))
+
+	print("\nParse Table:")
+	print(final_table)
+	print("\n")
+
+productions = ['E->E+T|T', 'T->T*F|F', 'F->(E)|e']
+start_symbol = 'E'
 
 print('Grammar:')
 print(productions)
 
-terminals = get_terminals(productions)
 non_terminals = get_non_terminals(productions)
-symbols = terminals + non_terminals
+terminals = get_terminals(productions, non_terminals)
 print('\nTerminals & Non-Terminals:')
 print(terminals)
 print(non_terminals)
-print(symbols)
 
 G = dict()
 terminals += '$'
+symbols = terminals + non_terminals
 
 for production in productions:
 	lhs = production.split('->')[0]
 	rhs = production.split('->')[1].split('|')
 	G[lhs] = rhs
 
-canonical_items, state_transitions = find_canonical_items(G, symbols)
+canonical_items, state_transitions = find_canonical_items(G, symbols, start_symbol)
 
-print(canonical_items)
+print('\nCanonical Items:')
+for line_no, line in enumerate(canonical_items):
+	print(str(line_no+1) + '\t', line)
 
-print(state_transitions)
+print('\nState Transitions:')
+for line_no, line in enumerate(state_transitions):
+	print(str(line_no+1) + '\t', line)
+
+accept_state, reductions = make_reductions(canonical_items, G, start_symbol)
+
+printTable()
